fe47406cf3958827bf9313da9f0484021fc41afe
Aditya Raj Gupta <adityaraj51202@gmail.com>
2022-09-10 18:36:02 +0530

    Updates

---
+++ precache-manifest.8e3cf18687728e401dbe206f53a12820.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "87439d9189fd02259bed",
+    "url": "/react-crud-app/static/css/main.358c2745.chunk.css"
+  },
+  {
+    "revision": "87439d9189fd02259bed",
+    "url": "/react-crud-app/static/js/main.87439d91.chunk.js"
+  },
+  {
+    "revision": "ea806013200e2a582bf2",
+    "url": "/react-crud-app/static/css/1.46a442cb.chunk.css"
+  },
+  {
+    "revision": "ea806013200e2a582bf2",
+    "url": "/react-crud-app/static/js/1.ea806013.chunk.js"
+  },
+  {
+    "revision": "9c524bd88a181e261976",
+    "url": "/react-crud-app/static/js/runtime~main.9c524bd8.js"
+  },
+  {
+    "revision": "d7c23f8c7a87c060a0582659d251ff4e",
+    "url": "/react-crud-app/index.html"
+  }
+];
